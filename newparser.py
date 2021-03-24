286c0116b386bd659bb054882982a1525fc30d6e
godlike375 <32139587+godlike375@users.noreply.github.com>
2021-03-24 17:11:58 +0300

    the final release

--- newparser.py
+++ newparser.py
@@ -2,7 +2,7 @@ import lexer as lex
 import re
 
 
-test = "a = 2 + g l=(1+(l*2+1)+a) if (c>0){ if(a>2){a = a+1 if(a>2){a = a+1} if(a>2){a = a+1} k=k+1 c=c+2}k = k+1}else{k=k+1}"
+test = "a = 2 + g l=((l*2+1)+a) if (c>0){ if(a>2){a = a+1 if(a>2){a = a+1} if(a>2){a = a+1} k=k+1 c=c+2}k = k+1}else{k=k+1}"
 
 print(test)
 print(lex.lexer(test))
@@ -20,13 +20,13 @@ def to_nodes(lexemes):
 grammar = \
 [
     (' ', ' WS '),
-    ('expr', 'VAR ASGN (LBR (NUM|VAR)( OP (NUM|VAR))* RBR)|((NUM|VAR)( OP (NUM|VAR))*)'),
+    ('varasgn', 'VAR ASGN'),
     ('value', 'NUM|VAR'),
-    ('math', '(LBR value( OP value)* RBR)|(value( OP value)*)'),
-    ('math', 'LBR math( OP math)* RBR'),
-    ('expr', 'value ASGN math'),
     ('log_exp', 'value LGOP value( LGOP value)*'),
     ('if_condition', 'LBR log_exp RBR'),
+    ('math', '(LBR value( OP value)* RBR)|(value( OP value)*)'),
+    ('math', 'LBR math( OP math)* RBR'),
+    ('expr', 'varasgn math'),
     ('body', 'LSBR(( expr)*( if_exp)*)* RSBR'),
     ('if_head', 'IF if_condition'),
     ('if_exp', 'if_head body( ELSE body)?')
@@ -42,45 +42,74 @@ class Node:
     def __repr__(self):
             return "({a}, {b})".format(a=self.name, b=self.token)
 
-def replace(lexemes):
+def remove_whitespaces(helper):
+    for j in range(len(helper)):
+        h2 = helper[j]
+        if len(h2) > 0 and h2[0] == ' ':
+            helper[j] = h2[1:]
+        if len(h2) > 0 and h2[len(h2) - 1] == ' ':
+            helper[j] = h2[:len(h2) - 1]
+
+def get_positions(m, helper):
+
+    start, end = int(m.start()), int(m.end())
+    new_helper = [helper[:start], helper[start:end], helper[end:]]
+
+    remove_whitespaces(new_helper)
+
+    fp_len = len(new_helper[0].split(' '))
+    sp_len = len(new_helper[1].split(' '))
+
+    return start, end, fp_len, sp_len
+
+
+
+def abstraction_up(AST):
+    changes = False
+    for g in grammar:
+        while True:
+            helper = get_helper_AST(AST)
+            matched = re.search(g[1], helper)
+            # суть в том, чтобы замапить массив лексем в строку из типов лексем:
+            # [Node('VAR','a'), Node('ASGN','=')] => 'VAR ASGN'
+            # по этой строке пройтись регулярками, найти совпадение (то есть найти один нетерминал)
+            # взять стартовый и конечный индекс совпадения и разрезать строку на 1-3 строки:
+            # 'IF LBR NUM LGOP NUM RBR' => 'IF LBR', 'NUM LGOP NUM', 'RBR'
+            # разделить строки через пробелы на массивы строк
+            # 'IF LBR', 'NUM LGOP NUM', 'RBR' => ['IF', 'LBR'], (a) ['NUM', 'LGOP', 'NUM'] (b), ['RBR]
+            # получшить индексы начала и конца (a, b) среднего массива
+            # и таким образом получить аналогичные индексы для оригинального массива Nodes
+            # далее в оригинальном массиве заменить по этим индексам несколько нод на новую
+            # а ля [Node(IF, if), Node(LBR, '(') Node(log_exp, [Node('NUM', '1'), Node('OP', '*')....]),  Node('RBR', ')')  ]
+            if matched != None:
+                changes = True
+                start, end, first_len, second_len = get_positions(matched, helper)
+                NEW_AST = []
+                if start != 0:
+                    NEW_AST.extend(AST[:first_len])
+                    NEW_AST.append(Node(g[0], AST[first_len:first_len + second_len]))
+                    if end != len(helper):
+                        NEW_AST.extend(AST[first_len + second_len:len(AST)])
+                else:
+                    NEW_AST.append(Node(g[0], AST[:second_len]))
+                    if end != len(helper):
+                        NEW_AST.extend(AST[second_len:len(AST)])
+
+                AST = NEW_AST
+
+            else:
+                break
+        print(helper)
+    return changes, AST
+
+
+
+def parse_AST(lexemes):
     lexemes = list(filter(lambda x: x[0]!='WS', lexemes))
     AST = to_nodes(lexemes)
     while True:
-        changes = False
-        for g in grammar:
-            while True:
-                helper = get_helper_AST(AST)
-                m = re.search(g[1], helper)
-                if m != None:
-                    changes = True
-                    start, end = int(m.start()), int(m.end())
-                    helper2 = [helper[:start], helper[start:end], helper[end:]]
-                    NEW_AST = []
-                    for j in range(len(helper2)):
-                        h2 = helper2[j]
-                        if len(h2)>0 and h2[0]==' ':
-                            helper2[j] = h2[1:]
-                        if len(h2)>0 and h2[len(h2)-1]==' ':
-                            helper2[j] = h2[:len(h2)-1]
-
-                    fp_len = len(helper2[0].split(' '))
-                    sp_len = len(helper2[1].split(' '))
-                    if start != 0:
-                        NEW_AST.extend(AST[:fp_len])
-                        NEW_AST.append(Node(g[0], AST[fp_len:fp_len + sp_len]))
-                        if end != len(helper):
-                            NEW_AST.extend(AST[fp_len + sp_len:len(AST)])
-                    else:
-                        sp_len = len(helper2[1].split(' '))
-                        NEW_AST.append(Node(g[0], AST[:sp_len]))
-                        if end != len(helper):
-                            NEW_AST.extend(AST[sp_len:len(AST)])
-
-                    AST = NEW_AST
-
-                else:
-                    break
-            print(helper)
+        changes, NEW_AST = abstraction_up(AST)
+        AST = NEW_AST
         if changes == False:
             break
 
@@ -104,6 +133,6 @@ def evaluate(lexemes):
 
 
 
-print(replace(tokens))
+print(parse_AST(tokens))
 
 
